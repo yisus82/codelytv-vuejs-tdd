383152bf76180cd3047aa14a97baf9dea48b2643
Jesús Ángel Pérez-Roca Fernández <djalma8@gmail.com>
2020-07-01 03:25:31 +0200

    Add loading spinner

--- src/app/shared-components/LoadSpinner.vue
+++ src/app/shared-components/LoadSpinner.vue
@@ -1,7 +1,8 @@
 <template>
-  <div v-if="isLoading" class="spinner">
+  <span v-if="isLoading" class="spinner">
+    <q-spinner-mat data-testid="loading" size="20"/>
     <p>Loading...</p>
-  </div>
+  </span>
 </template>
 
 <script>
@@ -11,4 +12,11 @@
 </script>
 
 <style>
+.spinner {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+}
 </style>
--- src/app/shared-components/__test__/LoadSpinner.spec.js
+++ src/app/shared-components/__test__/LoadSpinner.spec.js
@@ -7,9 +7,20 @@ describe('LoadSpinner.vue', () => {
     expect(wrapper.text()).not.toContain('Loading...')
   })
 
-  it('shows loading', () => {
+  it('shows loading text', () => {
     const wrapper = Wrap(LoadSpinner).withProps({ isLoading: true }).mount()
 
     expect(wrapper.text()).toContain('Loading...')
+  })
+
+  it('does not show loading spinner', () => {
+    const wrapper = Wrap(LoadSpinner).withProps({ isLoading: false }).mount()
+    expect(wrapper.find(`[data-testid="loading"]`).element).toBeUndefined()
+  })
+
+  it('shows loading spinner', async () => {
+    const wrapper = Wrap(LoadSpinner).withProps({ isLoading: true }).mount()
+
+    expect(wrapper.find(`[data-testid="loading"]`).element).not.toBeUndefined()
   })
 })
